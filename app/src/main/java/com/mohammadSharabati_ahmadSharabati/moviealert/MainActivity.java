45669b58ad9800e2f33c5e442c430a0bda02580e
mohammadSharabati <c9.mohammad.97@outlook.sa>
2020-01-13 15:18:59 +0200

    update

--- app/src/main/java/com/mohammadSharabati_ahmadSharabati/moviealert/MainActivity.java
+++ app/src/main/java/com/mohammadSharabati_ahmadSharabati/moviealert/MainActivity.java
@@ -2,13 +2,45 @@ package com.mohammadSharabati_ahmadSharabati.moviealert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addManuallyMovie ,searchMovie,calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addManuallyMovie = (Button) findViewById(R.id.btnAddManuallyMovie);
+        searchMovie = (Button) findViewById(R.id.btnSearchMovie);
+        calendar = (Button) findViewById(R.id.btnCalendar);
+
+        addManuallyMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,Add.class);
+                startActivity(signIn);
+            }
+        });
+
+        searchMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,Search.class);
+                startActivity(signIn);
+            }
+        });
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,Calendar.class);
+                startActivity(signIn);
+            }
+        });
     }
 }
