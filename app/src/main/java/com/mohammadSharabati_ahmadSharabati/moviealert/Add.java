a62b632a26ca4914f19d43b9eb43e61fb50a65da
mohammadSharabati <c9.mohammad.97@outlook.sa>
2020-01-13 20:00:25 +0200

    update

--- app/src/main/java/com/mohammadSharabati_ahmadSharabati/moviealert/Add.java
+++ app/src/main/java/com/mohammadSharabati_ahmadSharabati/moviealert/Add.java
@@ -1,22 +1,25 @@
 package com.mohammadSharabati_ahmadSharabati.moviealert;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class Add extends AppCompatActivity {
 
     private static final String TAG = "Add";
-    private TextView mDisplayDate;
+    private TextView mDisplayDate, time, RunTime;
+    private Button AddImage;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
@@ -25,7 +28,12 @@ public class Add extends AppCompatActivity {
         setContentView(R.layout.activity_add);
 
         mDisplayDate = (TextView) findViewById(R.id.date);
+        time = (TextView) findViewById(R.id.time);
+        RunTime = (TextView) findViewById(R.id.RunTime);
+        AddImage = (Button) findViewById(R.id.AddImage);
 
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Date
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,7 +46,7 @@ public class Add extends AppCompatActivity {
                         Add.this,
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                         mDateSetListener,
-                        year,month,day);
+                        year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
@@ -54,6 +62,69 @@ public class Add extends AppCompatActivity {
                 mDisplayDate.setText(date);
             }
         };
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Time
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get Current Time
+                final Calendar c = Calendar.getInstance();
+                int mHour = c.get(Calendar.HOUR_OF_DAY);
+                int mMinute = c.get(Calendar.MINUTE);
 
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        Add.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                time.setText(hourOfDay + ":" + minute);
+                            }
+                        }, mHour, mMinute, false);
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.show();
+            }
+        });
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Run Time
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        RunTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get Current Time
+                final Calendar c = Calendar.getInstance();
+                int mHour = c.get(Calendar.HOUR_OF_DAY);
+                int mMinute = c.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        Add.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                RunTime.setText(hourOfDay + ":" + minute);
+                            }
+                        }, mHour, mMinute, false);
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.show();
+            }
+        });
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Add Image
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        AddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
